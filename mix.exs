88f933ce21c91480e4243fd2c3a4c5074846cbaa
pyzlnar@Monochrome <pyzlnar@gmail.com>
2019-08-10 11:04:30 -0500

    :sparkles: Added mix dependencies

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bot.MixProject do
 
   def project do
     [
-      app: :bot,
+      app: :rem,
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
@@ -21,6 +21,7 @@ defmodule Bot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:alchemy, "~> 0.6.4", hex: :discord_alchemy}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
