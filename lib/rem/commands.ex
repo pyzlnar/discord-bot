b7372d5542fc205379c3bc22a618c6878053616e
pyzlnar@Monochrome <pyzlnar@gmail.com>
2019-08-10 13:49:36 -0500

    :sparkles: Added generic help command

--- lib/rem/commands.ex
+++ lib/rem/commands.ex
@@ -4,4 +4,13 @@ defmodule Rem.Commands do
   Cogs.def ping do
     Cogs.say "pong!"
   end
+
+  Cogs.def help do
+    Cogs.say """
+      WIP: Currently under development!
+      Commands:
+      !help Shows this menu
+      !ping Tests if im still alive
+    """
+  end
 end
